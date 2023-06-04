53909a616169b26a71775413fed94da01a3ee649
Shaun Moate <shaun.moate@gmail.com>
2023-06-04 22:03:12 +0100

    Agenda.UnitTests: added some VERY basic unit tests
    * Agenda.UnitTests: added some very basic unit tests

--- tests/nvim_agenda_spec.lua
+++ tests/nvim_agenda_spec.lua
@@ -1,7 +1,29 @@
 describe("nvim-agenda", function()
 
-  it("can be required", function()
-    require('nvim-agenda')
+  -- Globals
+  it("nvim-agenda can be required", function()
+    require("nvim-agenda")
+  end)
+
+  -- Highlight ("nvim-agenda.highlight")
+  it("highlight: can be required", function()
+    require("nvim-agenda.highlight")
+  end)
+
+  -- Config ("nvim-agenda.config")
+  it("config: can be required", function()
+    require("nvim-agenda.config")
+  end)
+
+  -- Colors ("nvim-agenda.colors")
+  it("colors: default theme can be found in the table", function()
+    local res = require("nvim-agenda.colors").default
+    assert(res)
+  end)
+
+  it("colors: gruvbox theme can be found in the table", function()
+    local res = require("nvim-agenda.colors").gruvbox
+    assert(res)
   end)
 
 end)
